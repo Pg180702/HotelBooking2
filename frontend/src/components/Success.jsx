332ab7c71eb0bc43c9d47097737320866106eabd
LAPTOP-K22GA1O8 <pranjalgangwar1807@gmail.com>
2024-05-18 17:29:06 +0530

    success

--- frontend/src/components/Success.jsx
+++ frontend/src/components/Success.jsx
@@ -27,41 +27,6 @@ const Success = () => {
           Yay!! Your Stay Has been Booked
           <br /> You Can view your booking under My Bookings
         </p>
-
-        <style jsx>{`
-          .card {
-            background: #ebf0f5;
-            text-align: center;
-            padding: 40px 0;
-          }
-          h1 {
-            color: #284b63;
-            font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
-            font-weight: 900;
-            font-size: 40px;
-            margin-bottom: 10px;
-          }
-          p {
-            color: #404f5e;
-            font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
-            font-size: 20px;
-            margin: 0;
-          }
-          i {
-            color: #284b63;
-            font-size: 100px;
-            line-height: 200px;
-            margin-left: -15px;
-          }
-          .card {
-            background: white;
-            padding: 60px;
-            border-radius: 4px;
-            box-shadow: 0 2px 3px #c8d0d8;
-            display: inline-block;
-            margin: 0 auto;
-          }
-        `}</style>
       </div>
     </div>
   );
