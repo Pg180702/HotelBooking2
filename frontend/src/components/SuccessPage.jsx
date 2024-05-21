5acbe769adf5e84f2ef0d74c730c7df1d31bd460
LAPTOP-K22GA1O8 <pranjalgangwar1807@gmail.com>
2024-05-21 10:44:45 +0530

    success

--- frontend/src/components/SuccessPage.jsx
+++ frontend/src/components/SuccessPage.jsx
@@ -1,19 +1,54 @@
 import React from "react";
-import "../App.css";
+
 const SuccessPage = () => {
   return (
-    <body>
-      <div class="card">
-        <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
-          <i class="checkmark">✓</i>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 100,
+      }}
+    >
+      <div
+        className="card"
+        style={{
+          backgroundColor: "white",
+          padding: "60px",
+          borderRadius: "4px",
+          boxShadow: "0 2px 3px #C8D0D8",
+          display: "inline-block",
+          margin: "0 auto",
+        }}
+      >
+        <div
+          style={{
+            borderRadius: "200px",
+            height: "200px",
+            width: "200px",
+            background: "#F8FAF5",
+            margin: "0 auto",
+          }}
+        >
+          <i
+            className="checkmark"
+            style={{
+              color: "#9ABC66",
+              fontSize: "100px",
+              lineHeight: "200px",
+              marginLeft: "-15px",
+            }}
+          >
+            ✓
+          </i>
         </div>
         <h1>Success</h1>
         <p>
-          Your Hotel Has Been Booked
-          <br /> Kindly Check Your Booking Under My Bookings
+          Yay!! Your Stay Has been Booked
+          <br /> You Can view your booking under My Bookings
         </p>
       </div>
-    </body>
+    </div>
   );
 };
 
